6552a0b521766ac8d0eb87393808c57cc6d1663b
bydavid1 <byronjimenez9911@gmail.com>
2021-06-09 23:05:57 -0600

    Tag page

--- api/posts/controllers/posts.js
+++ api/posts/controllers/posts.js
@@ -66,7 +66,5 @@ module.exports = {
         results: 0
       })
     }
-
-
   }
 };
--- api/tags/controllers/tags.js
+++ api/tags/controllers/tags.js
@@ -5,4 +5,37 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+
+  async menu(ctx) {
+     let entities = await strapi.query('tags').model.find({}, {_id:1,name:1,slug:1,logo:0}).limit(4)
+
+    ctx.send(entities)
+  },
+
+  async slug(ctx) {
+    let tag, results;
+    tag = await strapi.query('tags').model.findOne({slug: ctx.params.slug}).select({
+      _id: 1,
+      logo:0
+    })
+    console.log(tag)
+
+    if (tag) {
+      results = await strapi.query('posts').model.find({tags: tag._id}).select({
+        title: 1,
+        slug: 1,
+        date: 1,
+        cover: 1,
+        category: 1
+      }).populate('category', 'url')
+      ctx.send({
+        posts: results
+      })
+    } else {
+      ctx.send({
+        results: 0
+      })
+    }
+ },
+};
